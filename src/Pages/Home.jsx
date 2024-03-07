9d35dd550ae1dc1f9b070435ec6ea2b66c3e9705
Theryalexis <alexis62330@hotmail.fr>
2024-03-07 11:45:32 +0100

    creation du routage et des pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Feature from '../Components/Feature';
+import Hero from '../Components/Hero';
 
 const Home = () => {
     return (
         <main>
             <section>
+                <Hero />
                 <Feature />
             </section>
         </main>
