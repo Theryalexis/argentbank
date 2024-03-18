3081b15423b7c367959dde8f03a7f7a9061bacc7
Theryalexis <alexis62330@hotmail.fr>
2024-03-18 13:52:34 +0100

    Mise en place du provider

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { signIn } from '../Redux/api/callAuth.jsx';
 
 const Form = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignIn = async () => {
+    try {     
+      await signIn(username, password, dispatch, navigate);
+    } catch (error) {
+      setError("Erreur lors de la connexion. Veuillez vérifier vos identifiants.");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    await handleSignIn();
+  }; 
+
     return (
         <div>
-            <div class="input-wrapper">
-            <label for="username">Username</label
-            ><input type="text" id="username" />
-          </div>
-          <div class="input-wrapper">
-            <label for="password">Password</label
-            ><input type="password" id="password" />
-          </div>
-          <div class="input-remember">
-            <input type="checkbox" id="remember-me" /><label for="remember-me"
-              >Remember me</label
-            >
-          </div>
-          <a href="./users" class="sign-in-button">Sign In</a>
-          <button class="sign-in-button">Sign In</button>
+            <form onSubmit={handleSubmit}>
+                        <div className="input-wrapper">
+                            <label htmlFor="username" >Username</label>
+                            <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                        </div>
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div> 
+                        {error && <div className="error-message">{error}</div>}              
+                        <button type="submit" className="sign-in-button">Sign In</button>    
+                        
+                    </form>
+            
         </div>
     );
 };
