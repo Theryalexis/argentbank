242822449b223d4626c678aa8c97e5d8434c9ff7
Theryalexis <alexis62330@hotmail.fr>
2024-03-18 13:29:55 +0100

    Modification du redux

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,9 +1,11 @@
+// store.js
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './reducers/authSlice';
 
 const store = configureStore({
-    reducer: {
-      auth:,    
-    },  
-  });
-  
-  export default store;
+  reducer: {
+    auth: authReducer,    
+  },  
+});
+
+export default store;
