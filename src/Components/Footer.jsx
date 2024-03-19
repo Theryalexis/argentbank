88fd85a29b7018d78cc8fe0918b472921f770ccb
Theryalexis <alexis62330@hotmail.fr>
2024-03-19 12:43:27 +0100

    modif date footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="footer-text">Copyright 2020 Argent Bank</p>
+            <p className="footer-text">Copyright 2024 Argent Bank</p>
         </footer>
     );
 };
